0b9870732fb6dd34b6ef283a43e9aae801ce1f24
shard <alisher.sharipov2@mail.ru>
2023-05-05 02:21:20 +0600

    spring initial push

---
+++ src/main/java/pro/sky/CollectionsHomework/CollectionsHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.CollectionsHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/CollectionsHomework/CollectionsHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.CollectionsHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

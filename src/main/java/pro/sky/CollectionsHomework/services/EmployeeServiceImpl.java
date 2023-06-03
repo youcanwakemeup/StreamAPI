a1eb3b7c557609ea9d47ab041be2bbd16823e333
shard <alisher.sharipov2@mail.ru>
2023-06-03 16:42:07 +0600

    done

--- src/main/java/pro/sky/CollectionsHomework/services/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/CollectionsHomework/services/EmployeeServiceImpl.java
@@ -1,10 +1,12 @@
 package pro.sky.CollectionsHomework.services;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import pro.sky.CollectionsHomework.Employee;
 import pro.sky.CollectionsHomework.exceptions.EmployeeAlreadyAddedException;
 import pro.sky.CollectionsHomework.exceptions.EmployeeNotFoundException;
 import pro.sky.CollectionsHomework.exceptions.EmployeeStorageIsFullException;
+import pro.sky.CollectionsHomework.exceptions.NameIsNotCorrectException;
 
 import java.util.*;
 
@@ -16,12 +18,18 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee removeEmployee(String firstName, String lastName, int salary, int department) {
-        Employee employee = new Employee(firstName, lastName, salary, department);
-                if (employees.containsKey(employee.getFullName())) {
-                    return employees.remove(employee);
-
-                }
-        throw new EmployeeNotFoundException("Сотрудник не найден");
+        if (StringUtils.containsOnly(firstName, "[a-zA-Z]+") && StringUtils.containsOnly(lastName, "[a-zA-Z]+")) {
+            firstName = StringUtils.capitalize(firstName);
+            lastName = StringUtils.capitalize(lastName);
+            Employee employee = new Employee(firstName, lastName, salary, department);
+            if (employees.containsKey(employee.getFullName())) {
+                return employees.remove(employee);
+            }
+            throw new EmployeeNotFoundException("Сотрудник не найден");
+        }
+        else {
+            throw new NameIsNotCorrectException("Имя или фамилия не верны!");
+        }
     }
     @Override
     public Employee addEmployee(String firstName, String lastName, int salary, int department) {
